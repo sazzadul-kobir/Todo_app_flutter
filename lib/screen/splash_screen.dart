c2472a5a6223f5e3d10120c52b26ad2f631bac46
sazzad <kazisazzad805@gmail.com>
2024-01-09 23:26:58 +0600

    updated

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -5,6 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 import 'package:todoapp/constant/colors.dart';
 
+import 'home_screen.dart';
+
 
 class Splash extends StatefulWidget {
 
@@ -19,12 +21,12 @@ class _SplashState extends State<Splash> {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
 
-    // Future.delayed(Duration(seconds: 4), () {
-    //   Navigator.pushReplacement(
-    //     context,
-    //     MaterialPageRoute(builder: (context) => Home()), // Navigate to MainScreen
-    //   );
-    // });
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Home()), // Navigate to MainScreen
+      );
+    });
   }
 
   @override
@@ -43,19 +45,40 @@ class _SplashState extends State<Splash> {
 
       body: Center(
         child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Lottie.asset("assets/animation/anim.json",),
-              Text("Todo",style: TextStyle(
-                color: Colors.lightBlue,
-                fontSize: 50,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 6
-              ),),
-              // Lottie(composition: composition)
-            ],
-          ),
+
+
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+
+                Container(
+                  margin: EdgeInsets.only(bottom: 40),
+                    child: Lottie.asset("assets/animation/anim.json",)
+
+                ),
+
+
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    margin: EdgeInsets.only(top: 60),
+
+                    child: Text("Todo",style: TextStyle(
+
+                      color: Color(0xff023047),
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 6
+                    ),
+                    textAlign:TextAlign.center,
+                    ),
+                  ),
+                ),
+
+
+              ],
+            ),
+
         ),
       ),
     );
